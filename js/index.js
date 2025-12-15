7e59db54f67dc8904620a3aadafaacd0a7468b4e
mmtrudeau <marie.michele.trudeau.01@edu.cegeptr.qc.ca>
2025-12-15 18:56:56 -0500

    AJOUT : js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+<script>
+  window.addEventListener("load", () => {
+    document.getElementById("popup").style.display = "flex";
+  });
+
+  document.getElementById("closePopup").addEventListener("click", () => {
+    document.getElementById("popup").style.display = "none";
+  });
+</script>
